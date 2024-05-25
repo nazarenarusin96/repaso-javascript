33059ffd401d2bcbb1cba54d726fb4689ae04883
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-05-25 03:09:57 -0300

    Agregando la funcion try catch

---
+++ js/tryCatch.js
@@ -0,0 +1,17 @@
+//*permite manejar errores de tiempo de ejecución. Literalmente permite “intentar (try)” ejecutar el código y “atrapar (catch)” errores que pueden ocurrir en él.
+
+/*console.log(1+3);
+hola()
+
+console.log("probando");
+*/
+
+console.log(1+3);
+
+try{
+login()
+} catch(error){
+console.log(error);
+}
+
+console.log("probando");
