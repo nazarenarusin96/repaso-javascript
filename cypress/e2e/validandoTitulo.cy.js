e9026c36d6ce312c37b5f4655000c758d3284620
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-05-26 19:59:53 -0300

    Se actualizo el caso de prueba de validar titulo y se agrego la funcion enter

--- cypress/e2e/validandoTitulo.cy.js
+++ cypress/e2e/validandoTitulo.cy.js
@@ -4,11 +4,17 @@
 describe("Validando el titulo", ()=>{
 
     it("Test validando titulo", ()=>{
-        cy.visit("https://demoqa.com/text-box");
+        cy.visit("https://www.google.com/");
 
-        cy.title().should("eq", "ToolsQA")
+        cy.title().should("eq", "Google"); //? Key sensitive
 
-        console.log("ok");
+        cy.wait(1000) //? Espera el tiempo que se le indique
+
+        cy.log("Titulo validado");
+
+        cy.get("#APjFqb").type("cypress io {enter}"); //? Selecciona el input por id e ingresa texto, el {enter} siempre va dentro de las comillas
+
+        cy.get('.eKjLze').click(); //? Busca el link en el que se quiere ingresar por la primera clase y lo clickea
     })
 
 })
