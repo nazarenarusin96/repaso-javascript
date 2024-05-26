3a6da523bf5b794fd4b71332ce656db3a3dbd5b0
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-05-26 08:14:44 -0300

    Agregando casos de prueba de seleccion de lista con misma clase en select

--- cypress/e2e/selects.cy.js
+++ cypress/e2e/selects.cy.js
@@ -6,7 +6,11 @@ describe('Manejo de selects', ()=>{
     it('sección celulares movistar', ()=>{
         cy.visit('https://qa-practice.netlify.app/dropdowns');
 
-        cy.get('#dropdown-menu').select("Afghanistan");
+        cy.get('#dropdown-menu').select("Argentina");
+
+        //cy.get('#dropdown-menu').first().click(); //* Selecciona el primer elemento de una lista de elementos con la misma clase
+
+        //cy.get('#dropdown-menu').eq(3).click(); //* Selecciona el numero de elemento ingresado con la misma clase
     })
 
 })
