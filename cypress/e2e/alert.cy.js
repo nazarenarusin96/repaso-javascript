481e97de1dbbfca8178862520e01d479a8cf8815
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-05-29 13:12:39 -0300

    Agregando comentarios en el caso de prueba tablas y comenzando con los casos de prueba con alert

---
+++ cypress/e2e/alert.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+//* ^^ hace funcionar los comandos de cypress
+
+describe('', ()=>{
+
+    it('', ()=>{
+        cy.visit('');
+    })
+
+})
--- cypress/e2e/tablas.cy.js
+++ cypress/e2e/tablas.cy.js
@@ -10,34 +10,35 @@ describe('Test tablas', ()=>{
         cy.visit('https://qa-practice.netlify.app/pagination');
 
         //cy.get("#content > nav:nth-child(2) > ul").children(".page-item").should("contain","1").click()
-        cy.get("#content > nav:nth-child(2) > ul").children(".page-item").eq(1).click();
+        cy.get("#content > nav:nth-child(2) > ul").children(".page-item").eq(1).click(); //? Busca mediante el elemento hijo con la función .children()
 
-        cy.wait(1000)
+        cy.wait(1000) //espera
 
-        cy.get("#content > nav:nth-child(2) > ul").children(".page-item").eq(2).click();
+        cy.get("#content > nav:nth-child(2) > ul").children(".page-item").eq(2).click(); //! Se utiliza la función .eq(2) para que no genere error, busca los elementos por numero de array
 
-        cy.get("#content > nav:nth-child(2) > ul").children(".page-item").filter(".page-link").click();
+        cy.get("#content > nav:nth-child(2) > ul").children(".page-item").filter(".page-link").click(); //? Utiliza la función .filter() para filtrar por el elemento que se desea como una class
     })
 
     it('Elementos padre-hijo con for', ()=>{
         cy.visit('https://practice.expandtesting.com/add-remove-elements');        
 
-        cy.get(".container").children(".btn").click({force:true}).then(()=>{
+        cy.get(".container").children(".btn").click({force:true}).then(()=>{ //? Realizando una promesa con for para que genere varios botones en una sola función
 
             for(let i=1; i<=5; i++){
                 cy.get(".container").children(".btn").click({force:true})
             }
     })        
 
-    cy.wait(1000)
+    cy.wait(1000) //espera
 
 
-    cy.get(".container").find(".added-manually").then(()=>{
+    cy.get(".container").find(".added-manually").then(()=>{ //? Realizando una promesa con for para que elimine los botones generados antes en una sola función
 
-        let equal=0;
+        let equal=0; //* Se inicializa la variable equal para que el for pueda recorrer todos los botones
 
+        //* La siguiente función realiza clicks en botones mediante un for
         for(let i=1; i<=6; i++){
-            cy.get(".container").find(".added-manually").eq(equal).click({force:true})
+            cy.get(".container").find(".added-manually").eq(equal).click({force:true}) //? Se utiliza la función .find() para encontrar un elemento deseado como por class
             
         }
 
@@ -54,11 +55,11 @@ describe('Test tablas', ()=>{
             }
     })    
 
-        cy.get(".container").find(".added-manually").first().click({force:true})
+        cy.get(".container").find(".added-manually").first().click({force:true}) //? Busca el primer elemento
 
-        cy.wait(1000)
+        cy.wait(1000) //espera
 
-        cy.get(".container").find(".added-manually").last().click({force:true})
+        cy.get(".container").find(".added-manually").last().click({force:true}) //? Busca el último elemento
         
     })
 
@@ -72,7 +73,9 @@ describe('Test tablas', ()=>{
             }
     })
 
-    cy.get(".container").find(".added-manually").nextAll()
+    //! Esta función genera errores
+
+    cy.get(".container").find(".added-manually").nextAll() //? La función .nextAll() selecciona todos los elementos encontrados uno por uno como si fuera un for
         
     })
 
@@ -86,7 +89,9 @@ describe('Test tablas', ()=>{
             }
     })
 
-    |cy.get("[type='button']").parent().should("have.class",".container")
+        //! Esta función genera errores
+
+    |cy.get("[type='button']").parent().should("have.class",".container") //? La función .parent() busca por elemento padre, primero se indica que tipo de elemento es y busca por una caracteristica como la class
         
     })
 
