78e8bd2065e000ed2aec93e8930bc19048c6bf6f
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-06-10 15:42:29 -0300

    Comenzando con funciones

---
+++ jsAvanzado/js/4_Funciones/1_funciones.js
@@ -0,0 +1,25 @@
+//? Creando la función
+
+num1 = 20;
+
+num2="20;"
+
+//! Utilizar la primera
+function sumar (){
+    console.log(2+2);
+}
+
+//* Llamando la función
+sumar();
+
+const sumita = function (){
+    console.log(3+3);
+}
+
+sumita();
+
+function saludo(nombre, apellido){
+    console.log(`Nombre: ${nombre} - Apellido: ${apellido}`);
+}
+
+saludo("Nazarena", "Rusin");
