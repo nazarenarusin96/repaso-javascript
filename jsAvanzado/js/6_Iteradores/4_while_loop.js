388f81058d6687099f4f475d9adba19c24308599
Nazarena Rusin <rusinnazarenaf@gmail.com>
2024-06-14 12:07:20 -0300

    Terminando con iteradores y comenzando con arrays methods

--- jsAvanzado/js/6_Iteradores/4_while_loop.js
+++ jsAvanzado/js/6_Iteradores/4_while_loop.js
@@ -1,5 +1,24 @@
 let i=0;
 
+let a=0;
+
+
 while(i <=10){
     console.log("Número "+i);
+
+    i++;  //! SIEMPRE VA ESTO AL FINAL, SI NO ROMPE EL CÓDIGO
+}
+
+while(a<=50){
+    if(a%15 ===0){
+        console.log(`${a} FIZZBUZZ`); //! Siempre va primero la condición que menos se repite
+    }
+    else if(a%3 ===0){
+        console.log(`${a} fizz`);
+    }
+    else if(a%5 ===0){
+        console.log(`${a} buzz`);
+    }
+    i++
+    
 }
---
+++ jsAvanzado/js/6_Iteradores/5_do_while.js
@@ -0,0 +1,23 @@
+let i=0;
+
+//! Do while se ejecuta el menos una vez siempre, no importa si se cumple o no la condición
+
+do{
+
+    console.log(`Número: ${i}`);
+
+    i ++
+}while(i <= 20); //*condición
+
+console.log("######################################################");
+
+//? Aunque sea erroneo, siempre se va a ejecutar una vez
+
+let a=1000;
+
+do{
+
+    console.log(`Número: ${a}`);
+
+    i ++
+}while(a <= 20); //*condición
